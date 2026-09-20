3506840cad32461187ecb26b01a265bb012db95f
Konstantin <kozbeet@proton.me>
2026-09-20 23:34:49 +0300

    build: update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,28 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/rsschool-landing-page/',
+  plugins: [
+    {
+      name: 'move-public-image-paths',
+      transformIndexHtml(html) {
+        return html.replace(
+          /(src|href)="\/(slider|dark-theme|download|gallery|hero|icons|logo|menu)\//g,
+          '$1="/rsschool-landing-page/assets/images/$2/',
+        );
+      },
+    },
+  ],
   server: {
     host: '127.0.0.1',
   },
   build: {
     sourcemap: true,
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        menu: 'menu.html',
+      },
+    },
   },
 });
